f6d479e5becd14780b2ddca9d34cd7acc9507a7e
Kate Panchugina <e-k-a-t-e-r-i-n-a1@yandex.ru>
2022-09-16 12:26:45 +0300

    поиск максимального числа из трех

--- lesson1task4/Program.cs
+++ lesson1task4/Program.cs
@@ -1,1 +1,28 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Три числа и максимум из трех чисел
+//Подсказываем, что именно нужно сделать пользователю
+Console.Write("Введите число №1:");
+//ReadLine просит ввести данные, принимает в любом формате, Parse преобразует строку String в int
+int number_a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число №2:");
+int number_b = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число №3:");
+int number_c = int.Parse(Console.ReadLine());
+
+if (number_a>=number_b && number_a>=number_c)
+{
+Console.WriteLine($"Максимальное число = {number_a}");
+}
+if (number_b>=number_a && number_b>=number_c)
+{
+Console.WriteLine($"Максимальное число = {number_b}");
+}
+if (number_c>=number_b && number_c>=number_a)
+{
+Console.WriteLine($"Максимальное число = {number_c}");
+}
+if (number_a==number_b && number_a==number_c)
+{
+Console.WriteLine($"Числа равны");
+}
